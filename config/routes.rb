a1e8315bd65d41afacf23b3e0f5eb83e9d6d2983
Chase Lindsay <chase.lindsay1@gmail.com>
2020-02-20 16:28:19 -0300

    completed backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :cocktails, only: [:index, :new, :show, :create] do
-    resources :doses, only: [:new, :create, :delete]
+    resources :doses, only: [:create]
   end
+  resources :doses, only: [:destroy]
 end
