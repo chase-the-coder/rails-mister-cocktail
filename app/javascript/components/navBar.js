c4608a8104059c65856c7d3c00f717037171189a
Chase Lindsay <chase.lindsay1@gmail.com>
2020-02-21 16:14:57 -0300

    ingredient page

--- app/javascript/components/navBar.js
+++ app/javascript/components/navBar.js
@@ -1,25 +1,25 @@
 
-// const searchResult = (query) => {
-//   const results = document.getElementById("results");
-//   fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`)
-//     .then(response => response.json())
-//     .then((data) => {
-//       data.drinks.forEach((drink) => {
-//         const suggestion = `<li class="list-group-item" id="drink">${drink["strDrink"]}</li>`;
-//         results.insertAdjacentHTML('beforeend', suggestion);
-//       });
-//     });
-// };
+const searchResult = (query) => {
+  const results = document.getElementById("results");
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`)
+    .then(response => response.json())
+    .then((data) => {
+      data.drinks.forEach((drink) => {
+        const suggestion = `<li class="list-group-item" id="drink">${drink["strDrink"]}</li>`;
+        results.insertAdjacentHTML('beforeend', suggestion);
+      });
+    });
+};
 
-// const searchValue = () => {
-//   const search = document.getElementById("search");
-//   search.addEventListener('keyup', (event) => {
-//     event.preventDefault();
-//     results.innerHTML = '';
-//     searchResult(search.value);
-//     console.log(event);
-//   });
-// };
+const searchValue = () => {
+  const search = document.getElementById("search");
+  search.addEventListener('keyup', (event) => {
+    event.preventDefault();
+    results.innerHTML = '';
+    searchResult(search.value);
+    console.log(event);
+  });
+};
 
 // const searchResult = (query) => {
 //   const results = document.getElementById("results");
@@ -46,5 +46,5 @@
 //   const drink = document.getElementById("drink")
 // }
 
-// export {searchResult};
-// export {searchValue};
+export {searchResult};
+export {searchValue};
