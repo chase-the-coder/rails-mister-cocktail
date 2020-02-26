d878d6fdea8b77ae3890b9e893bd81c48769591f
Chase Lindsay <chase.lindsay1@gmail.com>
2020-02-26 15:48:33 -0300

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,6 @@
 require 'open-uri'
 require 'json'
 Ingredient.destroy_all
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 puts "Seeding started...."
 array = ("a".."z").to_a
 array.each do |letter|
